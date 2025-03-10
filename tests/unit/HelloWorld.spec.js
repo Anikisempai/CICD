852873310c16280636fd0abf6aaeb9a40c4a4848
anikisempai <anikisamayt@gmail.com>
2025-03-10 14:02:47 +0100

    s commit

--- tests/unit/HelloWorld.spec.js
+++ tests/unit/HelloWorld.spec.js
@@ -2,10 +2,10 @@ import { shallowMount } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld.vue';
 
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
+  it('it should render the props correctly', () => {
+    const msg = 'coucou';
     const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
+      propsData: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
   });
